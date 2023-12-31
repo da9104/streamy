01522a2b806acf73d5a8ac8bf4c4999686317eec
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-12-31 09:04:47 +0000

    modified Stripe api key error.

--- routes/billing.js
+++ routes/billing.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv')
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')(process.env.VITE_APP_SECRET_KEY)
 const requireLogin = require('../middlewares/requireLogin')
 dotenv.config();
 module.exports = app => {
