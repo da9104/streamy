d913ba8eda4cf5381a19819364247cdefdb1f392
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-12-31 01:42:13 +0000

    debugging Stripe api key.

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -6,15 +6,14 @@ import * as actions from '../actions'
 class Payment extends Component {
     render() {
         // debugger;
-        // console.log(__STRIPE_KEY__)
         return (
                 <StripeCheckout
                 name='Streamy'
                 description='Please do not refresh this page until it finishes.'
                 amount={100} 
                 token={token => this.props.handleToken(token)}
-               // stripeKey={import.meta.env.VITE_APP_STRIPE_KEY}
-                stripeKey={__STRIPE_KEY__}
+                stripeKey={import.meta.env.VITE_APP_STRIPE_KEY}
+               //  stripeKey={__STRIPE_KEY__}
                 >
                 <a className='px-4 py-1 text-white transition duration-500 ease-out bg-pink-200 rounded-lg hover:bg-pink-200 cursor-point text-sm font-medium'>Buy a credit</a>
               
